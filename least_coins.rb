cbf87683f814c2caba7223c2908c0b6a86809b18
Ave12304 <averyselover@hotmail.com>
2018-06-21 17:14:47 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -2,7 +2,27 @@
 #use def to find the amount of Coins
 #or use the if, else, elsif
 
-def least_coins()
-
+def least_coins(cents,coins = {"qaurters" => 25,"dimes" => 10,"nickles" => 5,"pennies" => 1})
+if cents%25 >= 0
+elsif cents%10 >= 0
+elsif cents%5 >= 0
+else cents%1 >= 0
 
 end
+def (%25 != 0) = "x"
+if number%25
+elsif cents%10 >= 0
+elsif cents%5 >= 0
+else cents%1 >= 0
+end
+end
+
+
+
+
+
+
+
+
+
+least_coins(56)
