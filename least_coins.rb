3d0166e01fa4f230ecbd5bc5d74ae1d2f2ffd24b
Ave12304 <averyselover@hotmail.com>
2018-06-21 18:31:06 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,6 @@
 #find the answer using the least amount of Coins
 #use def to find the amount of Coins
 #or use the if, else, elsif
-
 def least_coins(cents,coins = {"qaurters" => 25,"dimes" => 10,"nickles" => 5,"pennies" => 1})
 if cents%25 >= 0
 elsif cents%10 >= 0
@@ -9,13 +8,27 @@ elsif cents%5 >= 0
 else cents%1 >= 0
 
 end
-def (%25 != 0) = "x"
+def cents_25 (%25 != 0) = "x"
 if number%25
 elsif cents%10 >= 0
 elsif cents%5 >= 0
 else cents%1 >= 0
 end
 end
+
+def (%10 != 0) = "y"
+  if number%10
+  elsif cents%5 >= y
+  elsif cents%1 >= y
+  end
+
+  def (%5 != 0) = "z"
+    if number%5
+    elsif cents%1 >= z  
+  end       
+      
+    
+  
 
 
 
