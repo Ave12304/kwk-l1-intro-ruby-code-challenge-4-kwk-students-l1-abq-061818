795268df9ed4f5ac7b9adb7d579cc3ba2b986259
Ave12304 <averyselover@hotmail.com>
2018-06-21 16:02:09 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,8 @@
-#write out your code here
+#find the answer using the least amount of Coins
+#use def to find the amount of Coins
+#or use the if, else, elsif
 
-def least_coins(cents)
+def least_coins()
 
-#Code your answer here!
 
 end
